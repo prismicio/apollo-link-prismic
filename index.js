81d54c788feb111d60702e0548e3e04913fb01a8
Sebastien Renault <srenault.contact@gmail.com>
2019-09-09 16:20:09 +0200

    Force using CDN endpoint

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ import { HttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import Prismic from 'prismic-javascript';
 
+const PRISMIC_ENDPOINT_REG = /^(https?):\/\/([^.]+)\.(cdn.)?([^.]+\.[^.]+)\/graphql\/?$/;
+
+function parseURI(endpoint) {
+  const [, protocol, repository, cdn, domain] = endpoint.match(PRISMIC_ENDPOINT_REG);
+
+  if (protocol && repository && domain) {
+    return {
+      protocol,
+      repository,
+      domain,
+    };
+  }
+
+  return null;
+}
+
 export function PrismicLink({uri, accessToken}) {
-  const BaseURIReg = /^(https?:\/\/.+?\..+?\..+?)\/graphql\/?$/;
-  const matches = uri.match(BaseURIReg);
-  if (matches && matches[1]) {
-    const [_, baseURI] = matches;
+  const parsedURI = parseURI(uri);
+
+  if (parseURI) {
+    const { protocol, repository, domain } = parsedURI;
+    const baseURI = `${protocol}://${repository}.cdn.${domain}`;
     const prismicClient = Prismic.client(`${baseURI}/api`, { accessToken });
     const prismicLink = setContext(
       (request, options) => {
@@ -18,22 +35,22 @@ export function PrismicLink({uri, accessToken}) {
               ...options.headers,
               ...authorizationHeader
             }
-          }
-        })
+          };
+        });
       }
     );
 
     const httpLink = new HttpLink({
-      uri,
+      uri: `${baseURI}/graphql`,
       useGETForQueries: true,
     });
 
     return prismicLink.concat(httpLink);
   } else {
-    throw new Error(`${uri} isn't a valid Prismic GraphQL endpoint`)
+    throw new Error(`${uri} isn't a valid Prismic GraphQL endpoint`);
   }
 }
 
 export default {
   PrismicLink,
-}
+};
