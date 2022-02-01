d8f497326759a29ae6f043cbb980e2410398e495
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-01-31 16:20:28 -1000

    test: createPrismicLink options

--- test/createPrismicLink.test.ts
+++ test/createPrismicLink.test.ts
@@ -3,10 +3,8 @@ import {
 	ApolloLink,
 	execute,
 	FetchResult,
-	LinkResult,
 	gql,
 	GraphQLRequest,
-	Observable,
 } from "@apollo/client/core";
 import { Response } from "node-fetch";
 import * as sinon from "sinon";
@@ -15,6 +13,10 @@ import * as prismicT from "@prismicio/types";
 
 import { createPrismicLink } from "../src";
 
+interface LinkResult<T> {
+	result: FetchResult<T>;
+}
+
 const executeRequest = <T = unknown>(
 	link: ApolloLink,
 	request: GraphQLRequest,
@@ -22,13 +24,7 @@ const executeRequest = <T = unknown>(
 	const linkResult = {} as LinkResult<T>;
 
 	return new Promise<LinkResult<T>>((resolve, reject) => {
-		const terminatingLink = new ApolloLink((operation) => {
-			linkResult.operation = operation;
-
-			return Observable.of({ data: null });
-		});
-
-		execute(ApolloLink.from([link, terminatingLink]), request).subscribe(
+		execute(link, request).subscribe(
 			(result) => {
 				linkResult.result = result as FetchResult<T>;
 			},
@@ -42,38 +38,141 @@ const executeRequest = <T = unknown>(
 	});
 };
 
-test("creates an HTTP Link", async (t) => {
-	const link = createPrismicLink({
-		repositoryName: "qwerty",
-		fetch: sinon.stub().callsFake((url) => {
-			if (
-				url === new URL("/api/v2", prismic.getEndpoint("qwerty")).toString()
-			) {
-				const response: Partial<prismicT.Repository> = {
-					refs: [
-						{
-							ref: "master",
-							isMasterRef: true,
-							id: "master",
-							label: "Master",
-						},
-					],
-				};
+const repositoryResponse: Partial<prismicT.Repository> = {
+	refs: [
+		{
+			ref: "master",
+			isMasterRef: true,
+			id: "master",
+			label: "Master",
+		},
+	],
+};
 
-				return new Response(JSON.stringify(response));
-			} else {
-				t.log(url);
-			}
-		}),
-	});
+test("creates an HTTP Link", async (t) => {
+	const repositoryName = "qwerty";
+	const apiEndpoint = prismic.getEndpoint(repositoryName);
+	const graphQLEndpoint = prismic.getGraphQLEndpoint(repositoryName);
 
 	const query = gql`
 		query {
 			foo
 		}
 	`;
+	const compressedQuery = `{foo}`;
 
-	const result = await executeRequest(link, { query });
+	const fetch = sinon.stub().callsFake((url) => {
+		const instance = new URL(url);
 
-	t.log(result);
+		if (url === apiEndpoint) {
+			return new Response(JSON.stringify(repositoryResponse));
+		} else if (`${instance.origin}${instance.pathname}` === graphQLEndpoint) {
+			t.is(instance.searchParams.get("query"), compressedQuery);
+
+			return new Response(
+				JSON.stringify({
+					data: {
+						foo: "bar",
+					},
+				}),
+			);
+		} else {
+			return new Response("{}", { status: 404 });
+		}
+	});
+
+	const link = createPrismicLink({
+		repositoryName,
+		fetch,
+	});
+
+	await executeRequest(link, { query });
+
+	t.plan(1);
+});
+
+test("supports custom API endpoint (for Rest API)", async (t) => {
+	const repositoryName = "qwerty";
+	const apiEndpoint = "https://example.com/";
+	const graphQLEndpoint = prismic.getGraphQLEndpoint(repositoryName);
+
+	const query = gql`
+		query {
+			foo
+		}
+	`;
+	const compressedQuery = `{foo}`;
+
+	const fetch = sinon.stub().callsFake((url) => {
+		const instance = new URL(url);
+
+		if (url === apiEndpoint) {
+			return new Response(JSON.stringify(repositoryResponse));
+		} else if (`${instance.origin}${instance.pathname}` === graphQLEndpoint) {
+			t.is(instance.searchParams.get("query"), compressedQuery);
+
+			return new Response(
+				JSON.stringify({
+					data: {
+						foo: "bar",
+					},
+				}),
+			);
+		} else {
+			return new Response("{}", { status: 404 });
+		}
+	});
+
+	const link = createPrismicLink({
+		repositoryName,
+		apiEndpoint,
+		fetch,
+	});
+
+	await executeRequest(link, { query });
+
+	t.plan(1);
+});
+
+test("supports custom GraphQL endpoint", async (t) => {
+	const repositoryName = "qwerty";
+	const apiEndpoint = prismic.getEndpoint(repositoryName);
+	const graphQLEndpoint = "https://example.com/";
+
+	const query = gql`
+		query {
+			foo
+		}
+	`;
+	const compressedQuery = `{foo}`;
+
+	const fetch = sinon.stub().callsFake((url) => {
+		const instance = new URL(url);
+
+		if (url === apiEndpoint) {
+			return new Response(JSON.stringify(repositoryResponse));
+		} else if (`${instance.origin}${instance.pathname}` === graphQLEndpoint) {
+			t.is(instance.searchParams.get("query"), compressedQuery);
+
+			return new Response(
+				JSON.stringify({
+					data: {
+						foo: "bar",
+					},
+				}),
+			);
+		} else {
+			return new Response("{}", { status: 404 });
+		}
+	});
+
+	const link = createPrismicLink({
+		repositoryName,
+		graphQLEndpoint,
+		fetch,
+	});
+
+	await executeRequest(link, { query });
+
+	t.plan(1);
 });
